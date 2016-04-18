a856db43950fad86180210dfc880e254c6c189b6
Marcus Schoo <marcusschoo@gmail.com>
2016-04-19 09:10:11 +1200

    recipe command line tool

---
+++ recipe.py
@@ -0,0 +1,42 @@
+import argparse
+import plistlib
+
+def printNames(db):
+    for r in db:
+        print r['NAME']
+
+def parseMenu( file ):
+    menu = []
+    index = 0
+    for l in file.readlines():
+        if l.startswith('y '):
+            menu.append(index)
+        index = index + 1
+    return menu
+
+def printMenu(path_to_menu,db):
+    indices = parseMenu(path_to_menu)
+    for index in indices:
+        print '==============================='
+        print db[index]['NAME']
+        print '-------------------------------'
+        for i in db[index]['INGREDIENTS']:
+            print i['QUANTITY'], i['MEASUREMENT'], i['DESCRIPTION'], i['DIRECTION']
+        print '-------------------------------'
+        print db[index]['DIRECTIONS']
+    print '==============================='
+
+db=plistlib.readPlist('./recipes.plist')
+
+argparser = argparse.ArgumentParser(description="Command line recipe tool.")
+argparser.add_argument('-r','--recipes', dest='print_all_recipes', action='store_true',
+                       help='print a list of all recipes')
+argparser.add_argument('-s','--shoppinglist', dest='selected_recipes', action='store',
+                       type=argparse.FileType('r'),
+                       help='read in a file with selected recipes marked with "y " at the start of line and generate shopping list')
+args = argparser.parse_args()
+
+if args.print_all_recipes:
+    printNames(db)
+elif args.selected_recipes:
+    printMenu(args.selected_recipes,db)
