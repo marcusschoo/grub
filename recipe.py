5c977e443de16cfc6dddd10e66622b453ed9ae30
Marcus Schoo <marcusschoo@gmail.com>
2016-04-30 21:13:23 +1200

    Update

--- recipe.py
+++ recipe.py
@@ -38,14 +38,44 @@ def assign_recipe_ids(db):
         if not id:
             r['RECIPE_ID'] = str(uuid.uuid4())
 
+def getCollectionMembership(id):
+    result = ''
+    for c in collections:
+        if id in c:
+            result += 'Y'
+        else:
+            result += '-'
+    return result
+
+def printCollections():
+    for r in recipes:
+        id = r.get('RECIPE_ID', None)
+        if id:
+            print getCollectionMembership(id), r.get('NAME')
+
+def importCollections(file):
+    ri = 0
+    for l in file.readlines()
+        ci = 0
+        c.clear()
+        for c in collections:
+            if l[ci] == 'Y':
+                c.append(recipes[ri]['RECIPE_ID']
+            ci += 1
+        ri += 1
 
 db=plistlib.readPlist(FILE_PATH)
+recipes = db['RECIPES']
+collections = db['COLLECTIONS']
 
 argparser = argparse.ArgumentParser(description="Command line recipe tool.")
 argparser.add_argument('-r','--recipes', dest='print_all_recipes', action='store_true',
                        help='print a list of all recipes')
 argparser.add_argument('-c','--custom', dest='custom', action='store_true',
                        help='custom operation, changes frequently')
+argparser.add_argument('-i','--customInput', dest='custom_input', action='store',
+                       type=argparse.FileType('r'),
+                       help='Like custom but with file argument')
 argparser.add_argument('-s','--shoppinglist', dest='selected_recipes', action='store',
                        type=argparse.FileType('r'),
                        help='read in a file with selected recipes marked with "y " at the start of line and generate shopping list')
@@ -56,4 +86,7 @@ if args.print_all_recipes:
 elif args.selected_recipes:
     printMenu(args.selected_recipes,db)
 elif args.custom:
-    plistlib.writePlist({'RECIPES': db}, FILE_PATH)
+    #plistlib.writePlist({'RECIPES': db}, FILE_PATH)
+    printCollections()
+elif args.custom_input:
+    importCollections(args.custom_input)
