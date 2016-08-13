5b3b8396e32ba20cab9d30657bd65d9f937a535a
Marcus Schoo <marcusschoo@gmail.com>
2016-08-14 11:23:33 +1200

    add grub module

---
+++ grub/shopping_list.py
@@ -0,0 +1,23 @@
+class ShoppingList:
+
+    # from grub.shopping_list import ShoppingList
+    # my_shopping_list = ShoppingList(db)
+    # my_shopping_list.generate_form()
+    # my_shopping_list.read_form()
+    # my_shopping_list.generate_menu()
+    # my_shopping_list.generate_shopping_list()
+
+    def __init__(self, db):
+        self.db = db
+
+    def generate_form(self):
+        pass
+
+    def read_form(self):
+        pass
+
+    def generate_menu(self):
+        pass
+
+    def generate_shopping_list(self):
+        pass
