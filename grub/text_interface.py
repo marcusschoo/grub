1c94663d26474bee479f2eb76bed1c59ab41f83d
Jennifer Schoo <jennifer.i.schoo@gmail.com>
2016-08-21 15:32:44 +1200

    Text interface stub

---
+++ grub/text_interface.py
@@ -0,0 +1,16 @@
+
+class TextInterface:
+    def __init__(self):
+        print 'created TextInterface'
+
+    def append(self, line):
+        pass
+
+    def run(self):
+        pass
+
+    def get_line(self):
+        pass
+
+    def reset(self):
+        pass
