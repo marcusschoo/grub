a1f49f961377a4efec04bb1dc128a24a4d88c384
Jennifer Schoo <jennifer.i.schoo@gmail.com>
2016-08-21 16:34:52 +1200

    Added -C option stub

--- grub.py
+++ grub.py
@@ -1,11 +1,16 @@
+#!/usr/bin/python
+
 import argparse
 
 from grub.database import Database
+from grub.category import Category
 
 argparser = argparse.ArgumentParser(description="Command line to interact with the Grub database.")
 argparser.add_argument('grub_file', help='The grub file to read from')
 argparser.add_argument('-c','--category_edit', dest='edit_categories', action='store_true',
                        help='Edit the existing categories')
+argparser.add_argument('-C','--category_new', dest='new_category', action='store',
+                       help='Create a new category.')
 
 args = argparser.parse_args()
 
@@ -14,3 +19,6 @@ grub_db = Database(file_path=args.grub_file)
 if args.edit_categories:
     for c in grub_db.categories:
         print c
+elif args.new_category:
+	grub_category = Category(args.new_category)
+	grub_db.save_category(grub_category)
