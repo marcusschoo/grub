469426613cf44499d5dd84ce51c3dd40365d66c6
Jennifer Schoo <jennifer.i.schoo@gmail.com>
2016-08-21 15:59:00 +1200

    Implemented TextInterface class

--- grub/text_interface.py
+++ grub/text_interface.py
@@ -1,16 +1,42 @@
+import subprocess
+import tempfile
+import os
 
 class TextInterface:
     def __init__(self):
-        print 'created TextInterface'
+        self.file = tempfile.NamedTemporaryFile(mode='w+b',delete=False)
+        self.file_path = self.file.name
+        print 'Interfacing through temporary file:', self.file.name
 
     def append(self, line):
-        pass
+        self.file.write('%s\n' % line)
 
     def run(self):
-        pass
+        self.file.close()
+        subprocess.call(["open","-e","-W",self.file_path])
 
     def get_line(self):
-        pass
+        self.file = open(self.file_path)
+        for line in self.file:
+            yield line.strip()
+
+    def __del__(self):
+        self.reset()
+
+    def __enter__(self):
+        return self
+
+    def __exit__(self, exc_type, exc_value, traceback):
+        self.reset()
 
     def reset(self):
-        pass
+        os.remove(self.file_path)
+
+# Example Usage
+
+ti = TextInterface()
+ti.append('one')
+ti.append('two')
+ti.run()
+for line in ti.get_line():
+    print '>', line
