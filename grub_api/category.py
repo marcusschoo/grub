1b676c41849123175db310f02d220034ae6ec2c8
Marcus Schoo <marcusschoo@gmail.com>
2016-09-02 21:23:46 +1200

    added repr to category class

--- grub_api/category.py
+++ grub_api/category.py
@@ -3,3 +3,6 @@ class Category:
     def __init__(self, name):
         self.name = name
         self.recipes = []
+
+    def __repr__(self):
+        return '%s (%d)' % (self.name, len(self.recipes))
